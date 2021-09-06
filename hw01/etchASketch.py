1aadf6a3a751d73cdcdc9789c315b0603c6311e3
Geoffrey Tomlinson <tomlingb@rose-hulman.edu>
2021-09-06 03:41:00 +0000

    changed name of directory

---
+++ hw01/etchASketch.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import curses
+from curses import wrapper
+
+# stdscr = curses.initscr()
+# curses.noecho()
+# curses.cbreak()
+
+# stdscr.keypad(True)
+
+# sleep(10)
+# curses.nocbreak()
+# stdscr.keypad(False)
+# curses.echo()
+# curses.endwin
+
