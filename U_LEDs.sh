41e8c31e18300fb22a0227092b947179665be18d
Geoffrey Tomlinson <tomlingb@rose-hulman.edu>
2021-09-09 18:59:12 +0000

    Added flags to U_LEDs script

--- U_LEDs.sh
+++ U_LEDs.sh
@@ -2,16 +2,28 @@
 
 led=0
 value=0
-while getopts led:v: flag
+command="on"
+while getopts led:v:c: flag
 do
     case "${flag}" in
         led) led_number=${OPTARG};;
         v) value=${OPTARG};;
+	c) command=${OPTARG};;
     esac
 done
 
-
-
-echo working
+echo editing user LEDs
 cd /sys/class/leds/beaglebone\:green\:usr$led
-echo $value > invert
+
+if [ $command = "on" ]
+then
+	echo none > trigger 
+	echo 1 > brightness 
+elif [ $command = "invert" ]
+then 
+	echo $value > invert
+elif [ $command = "heartbeat" ]
+then
+	echo heartbeat > trigger
+fi
+
